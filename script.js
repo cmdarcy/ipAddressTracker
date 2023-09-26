9dca7a44312a7e95a86295c3e3fc18bbe83b57bc
Christopher d'Arcy <cdarcy656481@gmail.com>
2023-09-26 14:57:29 -0400

    add initial map functionality

--- script.js
+++ script.js
@@ -37,4 +37,15 @@ searchBtn.addEventListener("click", async (ev) => {
 		resultData.location.postalCode;
 	timezoneResult.innerText = "UTC" + resultData.location.timezone;
 	ispResult.innerText = resultData.isp;
+
+	let map = L.map("map").setView(
+		[resultData.location.lat, resultData.location.lng],
+		13
+	);
+
+	L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+		maxZoom: 19,
+		attribution:
+			'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	}).addTo(map);
 });
