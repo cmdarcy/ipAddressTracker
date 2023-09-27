75b54f5ce60727398a1d76cff25b4c7788e68d4b
Christopher d'Arcy <cdarcy656481@gmail.com>
2023-09-26 20:23:14 -0400

    add error handling and response, set inital map styling

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 async function getData(url) {
-	const response = await fetch(url);
-	const data = await response.json();
-	console.log(data);
-	return data;
+	try {
+		const response = await fetch(url);
+		if (response.ok) {
+			const data = await response.json();
+			console.log("Success", data);
+			return data;
+		} else {
+			console.log(`Server Error: `, response.statusText);
+		}
+	} catch (error) {
+		console.log("Fetch Error: ", error);
+	}
 }
+
 const ipRegex = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/g;
 
+const searchBar = document.querySelector("[data-search]");
 const searchBtn = document.querySelector("button");
 const ipAddressResult = document.querySelector("[data-ipAddress");
 const locationResult = document.querySelector("[data-location");
 const timezoneResult = document.querySelector("[data-timezone");
 const ispResult = document.querySelector("[data-isp");
 
-const map = L.map("map").setView([51.505, -0.09], 13);
+const map = L.map("map").setView([0, 0], 2);
 const myMarker = L.icon({
 	iconUrl: "./images/icon-location.svg",
 });
@@ -39,18 +49,27 @@ searchBtn.addEventListener("click", async (ev) => {
 			`https://geo.ipify.org/api/v2/country,city?apiKey=at_zTwfzUoygaKEGS2xfHWjvQlmfEaOJ&ipAddress=${searchPrompt}`
 		);
 	}
-	ipAddressResult.innerText = resultData.ip;
-	locationResult.innerText =
-		resultData.location.city +
-		", " +
-		resultData.location.region +
-		" " +
-		resultData.location.postalCode;
-	timezoneResult.innerText = "UTC" + resultData.location.timezone;
-	ispResult.innerText = resultData.isp;
 
-	map.setView([resultData.location.lat, resultData.location.lng], 13);
-	L.marker([resultData.location.lat, resultData.location.lng], {
+	if (typeof resultData === "undefined") {
+		searchBar.classList.add("invalid");
+		alert("Please use a valid domain or ip address!");
+		return;
+	} else {
+		searchBar.classList.remove("invalid");
+	}
+
+	const {
+		location: { city, region, postalCode, timezone, lat, lng },
+		ip,
+		isp,
+	} = resultData;
+	ipAddressResult.innerText = ip;
+	locationResult.innerText = city + ", " + region + " " + postalCode;
+	timezoneResult.innerText = "UTC" + timezone;
+	ispResult.innerText = isp;
+
+	map.setView([lat, lng], 13);
+	L.marker([lat, lng], {
 		icon: myMarker,
 		title: searchPrompt,
 	}).addTo(map);
